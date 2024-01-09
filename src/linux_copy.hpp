b3ad23b1734b350f8d034d6f606d3cb2bf256c71
KotoriAster <2549524927@qq.com>
2024-01-09 13:16:44 +0800

    final version

--- src/linux_copy.hpp
+++ src/linux_copy.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "LZ77.hpp"
+#include <iostream>
 #include <limits.h>
 #include <map>
 #include <regex>
 #include <string>
 #include <sys/stat.h>
+#include <vector>
 
 #define reg_file 1
 #define dir_file 2
@@ -75,12 +77,75 @@ private:
     std::map<ino_t, std::string> copied_inodes;
     std::map<std::string, std::string> copied_files;
   } ctx;
-  compressor_lz77 fb_compressor;
 
 public:
+  compressor_lz77 fb_compressor;
   file_backuper() {}
   file_backuper(compressor_lz77 compressor) : fb_compressor(compressor) {}
   void copy_file(const std::string &source, const std::string &destination);
   void copy_directory(const std::string &source, const std::string &destination,
                       const file_filter &para_filter);
 };
+
+inline void set_args(file_backuper &fb, file_filter &filter,
+                     std::vector<std::string> &args) {
+  if (!(args[2] == "-") && std::regex_match(args[2], std::regex("^[0-9]+$"))) {
+    filter.file_size_limit = std::stol(args[2]);
+  }
+
+  if (!(args[3] == "-") &&
+      std::regex_match(args[3], std::regex("^[rdsbcf-]+$"))) {
+    filter.file_type_limit = 0;
+    for (char &c : args[3]) {
+      if (flag_map.count(c)) {
+        filter.file_type_limit |= flag_map.at(c);
+      }
+    }
+  }
+
+  if (!(args[4] == "-")) {
+    filter.name_pattern = std::regex(args[4]);
+  }
+
+  if (!(args[5] == "-") && !(args[6] == "-")) {
+    struct tm start_time, end_time;
+    std::string start_time_string = args[5];
+    std::string end_time_string = args[6];
+    if (!strptime(start_time_string.c_str(), "%Y-%m-%d-%H:%M:%S", &start_time))
+      std::cerr << "Error parsing start time\n";
+    if (!strptime(end_time_string.c_str(), "%Y-%m-%d-%H:%M:%S", &end_time))
+      std::cerr << "Error parsing end time\n";
+    filter.file_time_limit = {mktime(&start_time), mktime(&end_time)};
+  }
+
+  compressor_lz77 compressor;
+  if (!(args[7] == "-")) {
+    compressor.compression_filter = std::regex(args[7]);
+  }
+
+  if (!(args[8] == "-") && std::regex_match(args[8], std::regex("^[0-9]+$"))) {
+    compressor.window_size = std::stoi(args[8]);
+  }
+
+  if (!(args[9] == "-") && std::regex_match(args[9], std::regex("^[0-9]+$"))) {
+    compressor.compress = std::stoi(args[9]);
+  }
+
+  fb.fb_compressor = compressor;
+}
+
+inline void args_printer(std::vector<std::string> &args) {
+  std::cout << "args: ";
+  std::cout << "\t source file: " << args[0]
+            << "\t destination file: " << args[1];
+  std::cout << "\n\t filter: ";
+  std::cout << "\n\t\t size: " << args[2];
+  std::cout << "\n\t\t type: " << args[3];
+  std::cout << "\n\t\t pattern: " << args[4];
+  std::cout << "\n\t\t start_time: " << args[5];
+  std::cout << "\n\t\t end_time: " << args[6];
+  std::cout << "\n\t compression: ";
+  std::cout << "\n\t\t pattern: " << args[7];
+  std::cout << "\n\t\t windows size: " << args[8];
+  std::cout << "\n\t\t IsCompress: " << args[9];
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "LZ77.hpp"
 #include "linux_copy.hpp"
 
+#include <cstdio>
 #include <ctime>
 #include <fstream>
 #include <iostream>
@@ -57,52 +58,32 @@ int main(int argc, char **argv) {
     return 1;
   }
   std::vector<std::string> args(argv + 1, argv + argc);
-  std::string source_string = args[0];
-  std::string destination_string = args[1];
+
+  file_backuper fb;
   file_filter filter;
-  if (!(args[2] == "-") && std::regex_match(args[2], std::regex("^[0-9]+$"))) {
-    filter.file_size_limit = std::stol(args[2]);
-  }
-
-  if (!(args[3] == "-") &&
-      std::regex_match(args[3], std::regex("^[rdsbcf-]+$"))) {
-    filter.file_type_limit = 0;
-    for (char &c : args[3]) {
-      if (flag_map.count(c)) {
-        filter.file_type_limit |= flag_map.at(c);
-      }
+  set_args(fb, filter, args);
+  int a = 0;
+  while (a != -1) {
+    int p;
+    std::string para;
+    std::cout << "\n position to change in args:\n";
+    scanf("%d", &p);
+    while (p > -1 && p < 10) {
+      std::cin >> para;
+      args.at(p) = para;
+      args_printer(args);
+      std::cout << "\n next position to change in args:\n";
+      scanf("%d", &p);
     }
+    args_printer(args);
+    std::cout << "\n Is the args right? 1 for yes, 0 for no\n";
+    scanf("%d", &a);
+    if (a) {
+      set_args(fb, filter, args);
+      fb.copy_directory(args[0], args[1], filter);
+    }
+    std::cout << "\n continue? -1 to exit\n";
+    scanf("%d", &a);
   }
-
-  if (!(args[4] == "-")) {
-    filter.name_pattern = std::regex(args[4]);
-  }
-
-  if (!(args[5] == "-") && !(args[6] == "-")) {
-    struct tm start_time, end_time;
-    std::string start_time_string = args[5];
-    std::string end_time_string = args[6];
-    if (!strptime(start_time_string.c_str(), "%Y-%m-%d-%H:%M:%S", &start_time))
-      std::cerr << "Error parsing start time\n";
-    if (!strptime(end_time_string.c_str(), "%Y-%m-%d-%H:%M:%S", &end_time))
-      std::cerr << "Error parsing end time\n";
-    filter.file_time_limit = {mktime(&start_time), mktime(&end_time)};
-  }
-
-  compressor_lz77 compressor;
-  if (!(args[7] == "-")) {
-    compressor.compression_filter = std::regex(args[7]);
-  }
-
-  if (!(args[8] == "-") && std::regex_match(args[8], std::regex("^[0-9]+$"))) {
-    compressor.window_size = std::stoi(args[8]);
-  }
-
-  if (!(args[9] == "-") && std::regex_match(args[9], std::regex("^[0-9]+$"))) {
-    compressor.compress = std::stoi(args[9]);
-  }
-
-  file_backuper fb = file_backuper(compressor);
-  fb.copy_directory(source_string, destination_string, filter);
   return 0;
 }
